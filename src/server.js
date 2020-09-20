ec623510d3025a0124944adc20cdf41a99a938c9
nikolasmelui <slak@samaradom.ru>
2020-09-20 17:51:49 +0400

    Add new server.js realization with express onboard

--- src/server.js
+++ src/server.js
@@ -1,20 +1,12 @@
 'use strict';
 
-const http = require('http');
-const { APPLICATION_HOST, APPLICATION_PORT } = require('./config');
-const { getApplicationHost, getApplicationPort } = require('./helpers');
+// Libs
+const express = require('express');
+// Configs
+const { APPLICATION } = require('./config');
 
-http
-  .createServer((req, res) => {
-    res.writeHead(200, 'OK', { 'Content-Type': 'text/plain' });
-    res.end(
-      `Hello from ${getApplicationHost(req)}:${getApplicationPort(
-        req,
-      )} and welcome to the nikolasmelui-node-boilerplate!`,
-    );
-  })
-  .listen(APPLICATION_PORT, APPLICATION_HOST, () =>
-    console.log(
-      `Server is listening on ${APPLICATION_HOST}:${APPLICATION_PORT}`,
-    ),
-  );
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello'));
+
+app.listen(APPLICATION.PORT, APPLICATION.HOST);
