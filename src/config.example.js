69c04a4f4da961e917ccb1313f4c47a79fa22802
nikolasmelui <slak@samaradom.ru>
2020-09-22 09:43:04 +0400

    Add auth payment logic

--- src/config.example.js
+++ src/config.example.js
@@ -1,14 +1,17 @@
-// const config = {
-//   APPLICATION: {
-//     HOST: '127.0.0.1',
-//     PORT: 3000,
-//   },
-//   PAYU: {
-//     URL: 'https://secure.snd.payu.com',
-//     CLIENT_ID: 0,
-//     CLIENT_SECRET: '',
-//     GRANT_TYPE: 'client_credentials',
-//   },
-// };
+const config = {
+  APPLICATION: {
+    HOST: '127.0.0.1',
+    PORT: 3000,
+  },
+  PAYU: {
+    URL: 'https://secure.snd.payu.com',
+    CLIENT_ID: 0,
+    CLIENT_SECRET: '',
+    GRANT_TYPE: 'client_credentials',
+    MERCHANT_POS_ID: '',
+    NOTIFY_URL: '',
+    CUSTOMER_IP: '',
+  },
+};
 
-// module.exports = config;
+module.exports = config;
