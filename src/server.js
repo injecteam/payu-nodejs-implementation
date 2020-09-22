de0b17e59e85c585a47412bc07735474bd2cd4be
nikolasmelui <slak@samaradom.ru>
2020-09-22 08:34:37 +0400

    Add static serving for express Add simple payment/auth endpoint Add simple payment/order endpoint

--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
 'use strict';
 
 // Libs
+const path = require('path');
 const express = require('express');
 // Configs
 const { APPLICATION } = require('./config');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.post('/payment/auth', (req, res) => {
+  res.send('Auth');
+});
+app.post('/payment/order', (req, res) => {
+  res.send('Auth');
+});
 
 app.listen(APPLICATION.PORT, APPLICATION.HOST);
